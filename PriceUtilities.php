ff36bda950c0e66bc2d0c5a5f98d06308b660c16
liqinfeng <345603222@qq.com>
2020-11-10 18:15:12 +0800

    trait 方法

---
+++ PriceUtilities.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 2020/11/10
+ * Time: 17:56
+ */
+
+trait PriceUtilities
+{
+    private $taxrate = 17;
+
+    public function calculateTax(float $price): float
+    {
+        return (($this->taxrate / 100) * $price));
+    }
+}
